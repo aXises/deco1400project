069561f180d5a434cbfc67fb854120c53ef1c247
aXises <leletonyli.13944@yahoo.com.au>
2017-03-09 17:53:13 +1000

    Header layout, menu layout and basic animations

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
 //JavaScript Document
+
+$(function(){
+    $(document).ready(function(){
+
+        //loading
+        console.log('dom ready');
+        loading();
+
+        function loading () {
+            $('.loading').addClass('hidden');
+            $('.loading').css('pointer-events','none');
+        }
+
+        // Navigation
+        $('.nav_button').click(function(){
+            $('.nav_overlay, nav').toggleClass('visible');
+        });
+
+    });
+});
