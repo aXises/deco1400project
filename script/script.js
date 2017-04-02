d94f97ea376d41cb6992aad085f04d7ae6f81b28
aXises <leletonyli.13944@yahoo.com.au>
2017-04-02 18:54:45 +1000

    Added navigation animations

--- script/script.js
+++ script/script.js
@@ -7,16 +7,32 @@ $(function(){
         console.log('dom ready');
         loading();
 
+        // Navigation-open
+        $('.nav_button').click(function(){
+            $(this).css("pointer-events", "none");
+            $(this).addClass('nav_button_hidden')
+            $('.nav_button_close').css("pointer-events", "all");
+            nav_toggle();
+        });
+
+        // Navigation-close
+        $('.nav_button_close').click(function(){
+            $(this).css("pointer-events", "none");
+            $('.nav_button').css("pointer-events", "all");
+            $('.nav_button').removeClass('nav_button_hidden')
+            nav_toggle();
+        });
+
+        function nav_toggle(){
+            $('.nav_overlay, nav').toggleClass('menuvisible');
+            $('#main, .nav_overlay').toggleClass('menuopen');
+            $('.nav_button_close').toggleClass('menutransform-open');
+        }
+
         function loading () {
             $('.loading').addClass('hidden');
             $('.loading').css('pointer-events','none');
         }
 
-        // Navigation
-        $('.nav_button').click(function(){
-            $('.nav_overlay, nav').toggleClass('menuvisible');
-            $('#main, .nav_overlay').toggleClass('menuopen');
-        });
-
-    });
-});
+    }); // End of document ready
+}); // End of use
