ac41ef775f148d6a12be84377d81ec1d82d39fbc
aXises <leletonyli.13944@yahoo.com.au>
2017-04-23 22:16:34 +1000

    Added page content and animations

--- script/script.js
+++ script/script.js
@@ -24,13 +24,63 @@ $(function(){
         });
 
         //Page scroll
+        var height = $(window).height();
+
         $('header aside').click(function(){
-            var height = $(window).height();
             $('body').animate({
                 scrollTop: ('+=%i', height)
             },700)
-            
         });
+
+        $('.to-top').click(function(){
+            $('body').animate({
+                scrollTop: 0
+            },700)
+        });
+
+        //Page animations
+        $(window).scroll(function(){
+            var position = $(window).scrollTop() + 50;
+            //Full height offset
+            if (position > height){
+                $('.nav_button, .numerals').css({
+                    'border':'#525252 solid 2px',
+                    'color':'#525252'
+                });
+            }
+            else {
+                $('.nav_button, .numerals').css({
+                    'border':'white solid 2px',
+                    'color':'white'
+                });
+            }
+
+            //Half height offset
+            if (position > height/2){
+                $('.to-top').css('right','0px')
+            }
+            else {
+                $('.to-top').css('right','-50px')
+            }
+
+            //Quarter height offset
+            if (position > height/4){
+                $('header aside').css('opacity','0');
+            }
+            else {
+                $('header aside').css('opacity','1');
+            }
+        });
+
+        //Other
+        $('.chapter span').hover(
+            function(){
+                $('.chapter').toggleClass('chapter-invert')
+            },
+            function(){
+                $('.chapter').toggleClass('chapter-invert')
+            }
+        );
 
         function nav_toggle(){
             $('.nav_overlay, nav').toggleClass('menuvisible');
