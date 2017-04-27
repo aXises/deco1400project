71ab70a31db55f9501cd71b58808b107141e8a38
aXises <leletonyli.13944@yahoo.com.au>
2017-04-27 19:29:23 +1000

    Page redirect script and loading fallback

--- script/script.js
+++ script/script.js
@@ -5,14 +5,24 @@ $(function(){
         //loading
         console.log('dom ready');
 
+        //Global variables
+        var loaded = 0 //Default page loading state
+        var light = true //Default light state
+        var height = $(window).height(); //Window height
+        var position = $(window).scrollTop() + 50; //User scroll position
+        var pos_right = $('.to-top').css('right'); //To-top right position
+
         //Wait for images
-        $(window).on("load", function() {
+        $(window).on("load", function(){
             loading();
         });
         
-        var light = true //Default light state
-        var height = $(window).height(); //Window height
-        var pos_right = $('.to-top').css('right'); //To-top right position
+        //Load after 5 seconds if images hasn't been loaded
+        setTimeout(function(){
+            if (loaded !== 1){
+                loading();
+            }
+        },5000)
 
         //Update height on resize
         $(window).resize(function(){
@@ -57,13 +67,43 @@ $(function(){
             },700)
         });
 
+        //Page change
+        $('nav a').click(function(event){
+            event.preventDefault();
+            var page = $(this).attr('href');
+            var pageUrl = window.location.href
+            var pageLocation = pageUrl.split('/').pop();
+            if (pageLocation === page){
+                if (position < height/4) {
+                    $('body').animate({
+                        scrollTop: ('+=%i', height)
+                    },700)
+                }
+                else {}
+            }
+            else {
+                $('body').fadeOut(750, redirect);
+            }
+
+            function redirect(){
+                window.location = page
+            }
+        });
+
+        $('#current_page').click(function(){
+            if (position > height/4){
+                $('body').animate({
+                    scrollTop: 0
+                },700)
+            }
+        });
+
         //Set section overlay to the element size
         var panelHeight = $('#panel1').height();
         $('#panel1_overlay').css('height', panelHeight);
 
         //Page animations
         $(window).scroll(function(){
-            var position = $(window).scrollTop() + 50;
             //Full height offset
             if (position > height && light == true){
                 $('.nav_button, .numerals').css({
@@ -83,7 +123,6 @@ $(function(){
                 $('.to-top').css('right','0px');
             }
             else {
-                console.log(pos_right)
                 $('.to-top').css('right', pos_right);
             }
 
@@ -115,6 +154,7 @@ $(function(){
         function loading(){
             $('.loading').addClass('hidden');
             $('.loading').css('pointer-events','none');
+            loaded = 1
         }
 
         function light_toggle(){
